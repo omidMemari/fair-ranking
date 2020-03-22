7a0a379967c03a2525cc1ecc19c95fc78fcad520
omidMemari <omid.memarrast@gmail.com>
2020-03-22 01:10:56 -0500

    added multiprocessing

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import multiprocessing
 from YahooDataReader import YahooDataReader
 from train import trainAdversarialRanking
 #from train import fair_adv_training
@@ -10,6 +11,7 @@ import torch
 import itertools
 from sklearn.model_selection import KFold
 import time
+
 
 start = time.time()
 # run your code
@@ -63,118 +65,94 @@ mu = 1e-2 # No need to change?
 #gamma = 2000
 n_splits = 3
 
-
-
-model_params_list = []
-lambdas_list = [0.01, 0.1, 1.0, 10.0, 12.0, 15.0, 20.0, 25.0, 50.0, 100.0] # somehow add 0.0 , division by zero!!
-plt_data_pl = np.zeros((len(lamdas_list)+1, 2))
-for i, lamda in enumerate(lamdas_list):
-        #args.lamda = lamda
-        #torch.set_num_threads(args.num_cores)
-        args.lambda_reward = 1.0
-        args.lambda_ind_fairness = 0.0
-        args.lambda_group_fairness = lamda
-        args.lr = 0.001
-        args.epochs = 10
-        args.progressbar = False
-        args.weight_decay = 0.0
-        args.sample_size = 25
-        args.optimizer = "Adam"
-       
-        for gamma in gammas_list:
-            args.gamma = gamma
-            #train_feats, train_rel = shuffle_combined(train_feats, train_rel)
+def myfunc(data):
+    
+    lamda, gamma = data
+    args.lambda_group_fairness = lamda
+    args.gamma = gamma
+    # prepare cross validation
+    k_fold = KFold(n_splits, True, 1)
+    cv_ndcg, cv_fair_loss = 0, 0
+    cv_theta = np.zeros(nf)
+    for fold_idx, (train_set, val_set) in enumerate(k_fold.split(dr_x)):
+        print("fold_idx: ", fold_idx)
+        print("train_set: ", train_set)
+        print("test_set: ", val_set)
+        model = trainAdversarialRanking(dr_x[train_set], dr_y[train_set], args=args)
+        results = testAdvarsarialRanking(dr_x[val_set], dr_y[val_set], model, args=args)
+        cv_ndcg += results["ndcg"]
+        cv_fair_loss += results["fair_loss"]
+        cv_theta += model["theta"]
+    cv_ndcg, cv_fair_loss, cv_theta = cv_ndcg/n_splits, cv_fair_loss/n_splits, cv_theta/n_splits
  
-            # prepare cross validation
-            k_fold = KFold(n_splits, True, 1)
-            # enumerate splits
-            #for train_index, val_index in kfold.split(dr_new[0]):
-            #    print('train: %s, test: %s' % (train_index, val_index))
-            cv_ndcg, cv_fair_loss = 0, 0
-            cv_theta = np.zeros(nf)
-            for fold_idx, (train_set, val_set) in enumerate(k_fold.split(dr_x)):
-                print("fold_idx: ", fold_idx)
-                print("train_set: ", train_set)
-                print("test_set: ", val_set)
-                model = trainAdversarialRanking(dr_x[train_set], dr_y[train_set], args=args)
-                results = testAdvarsarialRanking(dr_x[val_set], dr_y[val_set], model, args=args)
-                cv_ndcg += results["ndcg"]
-                cv_fair_loss += results["fair_loss"]
-                cv_theta += model["theta"]
-            cv_ndcg, cv_fair_loss, cv_theta = cv_ndcg/n_splits, cv_fair_loss/n_splits, cv_theta/n_splits
-            if cv_ndcg > best_ndcg:
-                best_gamma = gamma
-                best_ndcg = cv_ndcg
-                best_fair_loss = cv_fair_loss
-                best_theta = cv_theta
-        print("ndcg: {}, fair_loss: {}, lamda: {}, gamma: {}".format(results["ndcg"], results["fair_loss"], lamda, gamma))
-        #theta, Q = trainAdversarialRanking(X_train, Y_train, lamda, mu, gamma)
-        #model = fair_adv_training(dr, args=args)
-        #results = fair_adv_testing(model, vdr, args=args)
-        args.gamma = best_gamma
-        model["theta"] = best_theta
-        results = testAdvarsarialRanking(vdr_x ,vdr_y , model, args=args)
-        #plt_data_pl[i] = [results["ndcg"], results["avg_group_asym_disparity"]]
-        plt_data_pl[i] = [results["ndcg"], results["fair_loss"]]
-        print("ndcg: {}, fair_loss: {}, lamda: {}, gamma: {}".format(results["ndcg"], results["fair_loss"], lamda, best_gamma))
+    return lamda, gamma, cv_ndcg, cv_fair_loss, cv_theta
+
+def parallel_runs(data_list): 
+    pool = multiprocessing.Pool(processes=4)
+    #prod_x=partial(prod_xy, y=10) # prod_x has only one argument x (y is fixed to 10)
+    result_list = pool.map(myfunc, data_list)
+    print(result_list)
+
+
+
+data_list = list(itertools.product(lamdas_list, gammas_list))
+parallel_runs(data_list)
+
+# model_params_list = []
+# lambdas_list = [0.01, 0.1, 1.0, 10.0, 12.0, 15.0, 20.0, 25.0, 50.0, 100.0] # somehow add 0.0 , division by zero!!
+# plt_data_pl = np.zeros((len(lamdas_list)+1, 2))
+# for i, lamda in enumerate(lamdas_list):
+#         #args.lamda = lamda
+#         #torch.set_num_threads(args.num_cores)
+#         args.lambda_reward = 1.0
+#         args.lambda_ind_fairness = 0.0
+#         args.lambda_group_fairness = lamda
+#         args.lr = 0.001
+#         args.epochs = 10
+#         args.progressbar = False
+#         args.weight_decay = 0.0
+#         args.sample_size = 25
+#         args.optimizer = "Adam"
+       
+#         for gamma in gammas_list:
+#             args.gamma = gamma
+#             #train_feats, train_rel = shuffle_combined(train_feats, train_rel)
+ 
+#             # prepare cross validation
+#             k_fold = KFold(n_splits, True, 1)
+#             # enumerate splits
+#             #for train_index, val_index in kfold.split(dr_new[0]):
+#             #    print('train: %s, test: %s' % (train_index, val_index))
+#             cv_ndcg, cv_fair_loss = 0, 0
+#             cv_theta = np.zeros(nf)
+#             for fold_idx, (train_set, val_set) in enumerate(k_fold.split(dr_x)):
+#                 print("fold_idx: ", fold_idx)
+#                 print("train_set: ", train_set)
+#                 print("test_set: ", val_set)
+#                 model = trainAdversarialRanking(dr_x[train_set], dr_y[train_set], args=args)
+#                 results = testAdvarsarialRanking(dr_x[val_set], dr_y[val_set], model, args=args)
+#                 cv_ndcg += results["ndcg"]
+#                 cv_fair_loss += results["fair_loss"]
+#                 cv_theta += model["theta"]
+#             cv_ndcg, cv_fair_loss, cv_theta = cv_ndcg/n_splits, cv_fair_loss/n_splits, cv_theta/n_splits
+#             if cv_ndcg > best_ndcg:
+#                 best_gamma = gamma
+#                 best_ndcg = cv_ndcg
+#                 best_fair_loss = cv_fair_loss
+#                 best_theta = cv_theta
+#         print("ndcg: {}, fair_loss: {}, lamda: {}, gamma: {}".format(results["ndcg"], results["fair_loss"], lamda, gamma))
+#         #theta, Q = trainAdversarialRanking(X_train, Y_train, lamda, mu, gamma)
+#         #model = fair_adv_training(dr, args=args)
+#         #results = fair_adv_testing(model, vdr, args=args)
+#         args.gamma = best_gamma
+#         model["theta"] = best_theta
+#         results = testAdvarsarialRanking(vdr_x ,vdr_y , model, args=args)
+#         #plt_data_pl[i] = [results["ndcg"], results["avg_group_asym_disparity"]]
+#         plt_data_pl[i] = [results["ndcg"], results["fair_loss"]]
+#         print("ndcg: {}, fair_loss: {}, lamda: {}, gamma: {}".format(results["ndcg"], results["fair_loss"], lamda, best_gamma))
  
 end = time.time()
 elapsed = end - start
 print("time: ", elapsed)
-        #model = on_policy_training(dr, vdr, model, args=args)
-        #if i == 0:
-        #    results = evaluate_model(model, vdr, fairness_evaluation=False, group_fairness_evaluation=True, 
-        #                         deterministic=True, args=args, num_sample_per_query=20)
-        #    print(results)
-        #    plt_data_pl[0] = [results["ndcg"], results["avg_group_asym_disparity"]]
-        #results = evaluate_model(model, vdr, fairness_evaluation=False, group_fairness_evaluation=True, 
-        #                         deterministic=False, args=args, num_sample_per_query=20)
-        #print(results)
-        #model_params_list.append(model.w.weight.data.tolist()[0])
-        #print("Learnt model for lambda={} has model weights as {}".format(lgroup, model_params_list[-1]))
-        #plt_data_pl[i+1] = [results["ndcg"], results["avg_group_asym_disparity"]]
 
 
-
-
-#df = pd.DataFrame(list(train.data))
-#print(list(df.columns.values))
-
-
-##################################
-
-
-
-#result = []
-#plt_data_pl = np.zeros((len(lambdas_list)+1, 2))
-#for j, gamma in enumerate(gammas_list):
-#    for i, lamda in enumerate(lambdas_list):
-#        cv_util = [] # utility for different cross validations
-    
-        #model = LinearModel(D=args.input_dim)
-
-        #model = on_policy_training(dr, vdr, model, args=args)
-          
-#        theta, Q = trainAdversarialRanking(X_train, Y_train, lamda, mu, gamma)
-#        ndcg, fair_loss = testAdvarsarialRanking(X_train, Y_train, lamda, mu, gamma, theta)
-        #util = sum(cv_util) / len(cv_util)
-#        result.append([ndcg, fair_loss, lamda, gamma])
-#        print("ndcg: {}, fair_loss: {}, lamda: {}, gamma: {}".format(ndcg, fair_loss, lamda, gamma))
-#        if ndcg > best_ndcg:
-#            best_lamda = lamda
-#            best_gamma = gamma
-#            best_ndcg = ndcg
-#print("result: ", result)
-#print("best:: Utility: {}, lamda: {}, gamma: {}".format(best_ndcg, best_lamda, best_gamma))
-    
-    # if i == 0:
-    #     results = evaluate_model(model, vdr, fairness_evaluation=False, group_fairness_evaluation=True, 
-    #                         deterministic=True, args=args, num_sample_per_query=20)
-    #     print(results)
-    #     plt_data_pl[0] = [results["ndcg"], results["avg_group_asym_disparity"]]
-    # results = evaluate_model(model, vdr, fairness_evaluation=False, group_fairness_evaluation=True, 
-    #                         deterministic=False, args=args, num_sample_per_query=20)
-    # print(results)
-    # model_params_list.append(model.w.weight.data.tolist()[0])
-    # print("Learnt model for lambda={} has model weights as {}".format(lgroup, model_params_list[-1]))
-    # plt_data_pl[i+1] = [results["ndcg"], results["avg_group_asym_disparity"]]
