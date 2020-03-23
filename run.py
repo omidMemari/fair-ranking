6e2f374b157a9a5c3f83e70607371721be11f4e8
omidMemari <omid.memarrast@gmail.com>
2020-03-22 23:48:14 -0500

    added policy learning and plt

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import numpy as np
 import multiprocessing
+import matplotlib.pyplot as plt
+from matplotlib import colors, ticker, cm
+
+from pprint import pprint
+from progressbar import progressbar
+
 from YahooDataReader import YahooDataReader
 from train import trainAdversarialRanking
 #from train import fair_adv_training
@@ -12,6 +18,31 @@ import itertools
 from sklearn.model_selection import KFold
 import time
 
+from train_yahoo_dataset import on_policy_training
+from YahooDataReader import YahooDataReader
+from models import NNModel, LinearModel
+from evaluation import evaluate_model
+
+#########################################################################
+# #%matplotlib inline
+# from IPython.display import set_matplotlib_formats
+# set_matplotlib_formats('pdf', 'png')
+# plt.rcParams['savefig.dpi'] = 75
+
+# plt.rcParams['figure.autolayout'] = False
+# plt.rcParams['figure.figsize'] = 10, 6
+# plt.rcParams['axes.labelsize'] = 14
+# plt.rcParams['axes.titlesize'] = 16
+# plt.rcParams['font.size'] = 16
+# plt.rcParams['lines.linewidth'] = 2.0
+# plt.rcParams['lines.markersize'] = 8
+# plt.rcParams['legend.fontsize'] = 14
+
+# plt.rcParams['text.usetex'] = True
+# plt.rcParams['font.family'] = "serif"
+# plt.rcParams['font.serif'] = "cm"
+# plt.rcParams['text.latex.preamble'] = r"\usepackage{subdepth}, \usepackage{type1cm}"
+#####################################################################################
 
 start = time.time()
 # run your code
@@ -30,7 +61,7 @@ class Namespace:
         self.__dict__.update(kwargs)
 args = Namespace(conditional_model=True, gpu_id=None, progressbar=True, evaluate_interval=250, input_dim=29, 
                  eval_rank_limit=1000,
-                fairness_version="asym_disparity", entropy_regularizer=0.0, save_checkpoints=False, num_cores=1,
+                fairness_version="asym_disparity", entropy_regularizer=0.0, save_checkpoints=False, num_cores=8,
                 pooling='concat_avg', dropout=0.0, hidden_layer=8, summary_writing=False, 
                  group_fairness_version="asym_disparity",early_stopping=False, lr_scheduler=False, 
                  validation_deterministic=False, evalk=1000, reward_type="ndcg", baseline_type="value", 
@@ -39,7 +70,7 @@ args = Namespace(conditional_model=True, gpu_id=None, progressbar=True, evaluate
 torch.set_num_threads(args.num_cores)
 args.progressbar = False 
 
-args.group_feat_id = 4
+args.group_feat_id = 3
 args.mu = 1e-2
 
 dr_x = np.array(dr.data[0][:10])
@@ -49,8 +80,8 @@ vdr_y = np.array(vdr.data[1][:10])
 
 nc,nn,nf = np.shape(dr_x)
 
-lamdas_list = [500 ,1000, 2000, 3000, 4000]#[1e-3, 1e-2, 1e-1, 1e0, 1e1] #lambdas_list = [0.0, 0.1, 1.0, 10.0, 12.0, 15.0, 20.0, 25.0, 50.0, 100.0]
-gammas_list = [1e-2, 1e-1, 1e0, 1e1, 1e2]
+lamdas_list = [500, 2000]#[500 ,1000, 2000, 3000, 4000]#[1e-3, 1e-2, 1e-1, 1e0, 1e1] #lambdas_list = [0.0, 0.1, 1.0, 10.0, 12.0, 15.0, 20.0, 25.0, 50.0, 100.0]
+gammas_list = [1e-2]#[1e-2, 1e-1, 1e0, 1e1, 1e2]
 best_lamda = -1.0
 best_ndcg = -1.0
 mu = 1e-2 # No need to change?
@@ -92,7 +123,7 @@ def test_func(data):
 
 def parallel_runs(data_list):
     ls = []
-    pool = multiprocessing.Pool(processes=54)
+    pool = multiprocessing.Pool(processes=args.num_cores)
     #prod_x=partial(prod_xy, y=10) # prod_x has only one argument x (y is fixed to 10)
     result_list = pool.map(train_func, data_list)
     res = {key : [result_list[idx][1:5]
@@ -101,68 +132,83 @@ def parallel_runs(data_list):
     for key, values in res.items():
         ls.append([(key,values[idx]) for idx in range(len(res[key])) if  values[idx][1]==max([x[1] for x in values])])
    
-    pool = multiprocessing.Pool(processes=54)
+    pool = multiprocessing.Pool(processes=args.num_cores)
     result_list = pool.map(test_func, ls)
     print(result_list)
+    sorted_result = sorted(result_list, key=lambda x: x[0])
+    return sorted_result
 
-
+def ndcg_vs_disparity_plot(plt_data_mats, names, join=False, ranges=None):
+    plt.figure(figsize=(6.5, 4))
+    if ranges:
+        plt.xlim(ranges[0])
+        plt.ylim(ranges[1])
+    #initialize_params() ##Omid
+    for i, plt_data_mat in enumerate(plt_data_mats):
+        if not join:
+            plt.scatter(
+                plt_data_mats[i][:, 0],
+                plt_data_mats[i][:, 1],
+                marker="*",
+                label=names[i])
+        else:
+            plt.plot(
+                plt_data_mats[i][:, 0],
+                plt_data_mats[i][:, 1],
+                marker="*",
+                linestyle='--',
+                label=names[i])
+    plt.legend(fontsize=14)
+#     plt.title("Utility-Fairness trade-off",y=-0.30)
+    plt.xlabel("NDCG")
+    plt.ylabel(r'$\hat{\mathcal{D}}_{\rm group}$', fontsize=16)
+    plt.grid()
+    plt.savefig('./plots/german_tradeoff.pdf', bbox_inches='tight')
+    plt.show()
 
 data_list = list(itertools.product(lamdas_list, gammas_list))
-parallel_runs(data_list)
+adv_result = parallel_runs(data_list)
+plt_data_adv = [[adv_result[i][1], adv_result[i][2]] for i in range(len(adv_result))]
+print("plt_data_adv: ", plt_data_adv)
+#############################################################
+args.group_feat_id = 3
+model_params_list = []
+lambdas_list = [10.0, 20.0]#[0.0, 0.1, 1.0, 10.0, 12.0, 15.0, 20.0, 25.0, 50.0, 100.0]
+plt_data_pl = np.zeros((len(lambdas_list)+1, 2))
+for i, lgroup in enumerate(lambdas_list):
+        torch.set_num_threads(args.num_cores)
+        args.lambda_reward = 1.0
+        args.lambda_ind_fairness = 0.0
+        args.lambda_group_fairness = lgroup
+        
+        args.lr = 0.001
+        args.epochs = 10
+        args.progressbar = False
+        args.weight_decay = 0.0
+        args.sample_size = 25
+        args.optimizer = "Adam"
 
-# model_params_list = []
-# lambdas_list = [0.01, 0.1, 1.0, 10.0, 12.0, 15.0, 20.0, 25.0, 50.0, 100.0] # somehow add 0.0 , division by zero!!
-# plt_data_pl = np.zeros((len(lamdas_list)+1, 2))
-# for i, lamda in enumerate(lamdas_list):
-#         #args.lamda = lamda
-#         #torch.set_num_threads(args.num_cores)
-#         args.lambda_reward = 1.0
-#         args.lambda_ind_fairness = 0.0
-#         args.lambda_group_fairness = lamda
-#         args.lr = 0.001
-#         args.epochs = 10
-#         args.progressbar = False
-#         args.weight_decay = 0.0
-#         args.sample_size = 25
-#         args.optimizer = "Adam"
-       
-#         for gamma in gammas_list:
-#             args.gamma = gamma
-#             #train_feats, train_rel = shuffle_combined(train_feats, train_rel)
- 
-#             # prepare cross validation
-#             k_fold = KFold(n_splits, True, 1)
-#             # enumerate splits
-#             #for train_index, val_index in kfold.split(dr_new[0]):
-#             #    print('train: %s, test: %s' % (train_index, val_index))
-#             cv_ndcg, cv_fair_loss = 0, 0
-#             cv_theta = np.zeros(nf)
-#             for fold_idx, (train_set, val_set) in enumerate(k_fold.split(dr_x)):
-#                 print("fold_idx: ", fold_idx)
-#                 print("train_set: ", train_set)
-#                 print("test_set: ", val_set)
-#                 model = trainAdversarialRanking(dr_x[train_set], dr_y[train_set], args=args)
-#                 results = testAdvarsarialRanking(dr_x[val_set], dr_y[val_set], model, args=args)
-#                 cv_ndcg += results["ndcg"]
-#                 cv_fair_loss += results["fair_loss"]
-#                 cv_theta += model["theta"]
-#             cv_ndcg, cv_fair_loss, cv_theta = cv_ndcg/n_splits, cv_fair_loss/n_splits, cv_theta/n_splits
-#             if cv_ndcg > best_ndcg:
-#                 best_gamma = gamma
-#                 best_ndcg = cv_ndcg
-#                 best_fair_loss = cv_fair_loss
-#                 best_theta = cv_theta
-#         print("ndcg: {}, fair_loss: {}, lamda: {}, gamma: {}".format(results["ndcg"], results["fair_loss"], lamda, gamma))
-#         #theta, Q = trainAdversarialRanking(X_train, Y_train, lamda, mu, gamma)
-#         #model = fair_adv_training(dr, args=args)
-#         #results = fair_adv_testing(model, vdr, args=args)
-#         args.gamma = best_gamma
-#         model["theta"] = best_theta
-#         results = testAdvarsarialRanking(vdr_x ,vdr_y , model, args=args)
-#         #plt_data_pl[i] = [results["ndcg"], results["avg_group_asym_disparity"]]
-#         plt_data_pl[i] = [results["ndcg"], results["fair_loss"]]
-#         print("ndcg: {}, fair_loss: {}, lamda: {}, gamma: {}".format(results["ndcg"], results["fair_loss"], lamda, best_gamma))
- 
+        model = LinearModel(D=args.input_dim)
+
+        model = on_policy_training(dr, vdr, model, args=args)
+        if i == 0:
+            results = evaluate_model(model, vdr, fairness_evaluation=False, group_fairness_evaluation=True, 
+                                 deterministic=True, args=args, num_sample_per_query=20)
+            print(results)
+            plt_data_pl[0] = [results["ndcg"], results["avg_group_asym_disparity"]]
+        results = evaluate_model(model, vdr, fairness_evaluation=False, group_fairness_evaluation=True, 
+                                 deterministic=False, args=args, num_sample_per_query=20)
+        print(results)
+        model_params_list.append(model.w.weight.data.tolist()[0])
+        print("Learnt model for lambda={} has model weights as {}".format(lgroup, model_params_list[-1]))
+        plt_data_pl[i+1] = [results["ndcg"], results["avg_group_asym_disparity"]]
+
+###############################################################
+
+ndcg_vs_disparity_plot([plt_data_adv, plt_data_pl], ["Robust_Fair ($\lambda \in [0, 0.2]$)",
+                      "Policy_Ranking($\lambda \in [0,100]$ )"], join=True, ranges=[[0.60, 0.85], [0.00, 0.054]])
+
+
 end = time.time()
 elapsed = end - start
 print("time: ", elapsed)
